ef6d3b849139c02e5fe09e41f2a2f154cc867edf
GabutCrypto <107311331+GabutCrypto@users.noreply.github.com>
2022-06-21 20:33:34 +0700

    Add files via upload

---
+++ for(tiktok).py.c
@@ -0,0 +1,17 @@
+
+
+
+
+#projekan GabutCrypto
+
+nama_ban = ["zifilia","kangen ban",
+            "ungu","peterpan"]
+            
+kumpulan_lagu = ["gaaaaaa","giiiiii",
+                "guuuuu","gooooo"]
+
+for index,ban in enumerate(nama_ban):
+    print(index,":",ban)
+   
+for index,lagu in zip(nama_ban,kumpulan_lagu):
+    print (ban,"bernyanyi lagu :",lagu)
